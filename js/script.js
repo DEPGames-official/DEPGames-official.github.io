43a6e326073a1386d1af1901a574e18c70009a8c
codeylee05 <mlefa5002@gmail.com>
2023-05-26 16:53:37 +0200

    update html and css

--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
 
-//Lines 3 to 14 are code that allows the nav-link which the user is on to be styled differently according to the .active class in css
-currentUrl = window.location.href;
-var navLinks = document.getElementsByClassName("nav-links");
-
+//Lines 3 to 10 are code that allows the nav-link which the user is on to be styled differently according to the .active class in css
 var currentURL = window.location.href;
-
 var navLinks = document.getElementsByClassName('nav-links');
 
 for (var i = 0; i < navLinks.length; i++) {
